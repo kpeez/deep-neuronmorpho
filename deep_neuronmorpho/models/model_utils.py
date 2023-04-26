b3f233a306c510f1513de159d2bbeeab4043e4e6
Kyle Puhger <krpuhger@gmail.com>
2023-04-26 11:05:22 -0700

    Introduce GINBlock Class, Utility Functions and MLP Refactor (#5)
    * refactor: Simplify MLP implementation, remove gin_block function, and update docstrings
    
    - Simplified `MLP` by replacing the nn.ModuleList with an nn.Sequential container for the MLP layers.
    
    - The `gin_block` function has been removed.
    
    - The class and method docstrings have also been updated to reflect the changes made to the implementation.
    
    * move to parent directory
    
    * Add `GINBlock` class for GNN implementation
    
    * feat: Add module with pooling layer creation and tensor aggregation
    
    This commit introduces a new module containing utility functions for
    model-related tasks. The module currently includes two functions:
    
    1. create_pooling_layer: Creates a pooling layer for the given pooling type
       ('sum', 'mean', or 'max').
    
    2. aggregate_tensors: Aggregates tensor data according to the specified
       aggregation method ('cat', 'mean', 'max', 'sum', or 'wsum').
    
    * rename: aggregation config variables
    
    * rename aggregation config variables, add attrs_streams
    
    * Add create_gnn_layers function
    
    - Add a function to create a list of GNN layers (GINBlocks) for a given configuration.
    
    * Add load_attrs_streams function
    
    - Add a function to load attribute streams from a
    dictionary containing the range of indices.
    
    * rename `aggregation_type` to `neighbor_aggregation`
    
    - This commit renames `aggregation_type` to `neighbor_aggregation` in model config.
    - This is done to to improve clarity that this parameter describes
    how neighboring nodes are aggregated within GNN layers
    - `stream_aggregation` is set to "sum" until "wsum" is implemented

---
+++ deep_neuronmorpho/models/model_utils.py
@@ -0,0 +1,99 @@
+"""Utility functions for model-related tasks."""
+
+from functools import partial
+
+import torch
+from dgl.nn.pytorch import AvgPooling, MaxPooling, SumPooling
+from torch import Tensor, nn
+
+
+def load_attrs_streams(attrs_streams: dict[str, str]) -> dict[str, list[int]]:
+    """Load attribute streams from a dictionary containing the range of indices.
+
+    Args:
+        attrs_streams (dict[str, str]): A dictionary where the keys are stream names,
+        and the values are a tuple the index range (inclusive) in string format.
+
+    Returns:
+        dict[str, list[int]]: A dictionary where the keys are stream names,
+        and the values are lists of indices corresponding to the given index range.
+    """
+    attrs_idxs = {}
+    for name, stream in attrs_streams.items():
+        start, end = eval(stream)
+        attrs_idxs[name] = list(range(start, end + 1))
+
+    return attrs_idxs
+
+
+def create_pooling_layer(pooling_type: str) -> nn.Module:
+    """Create a pooling layer for the given pooling type.
+
+    Args:
+        pooling_type (str): The pooling type ('sum', 'mean', or 'max').
+
+    Returns:
+        nn.Module: The created pooling layer.
+
+    Raises:
+        NotImplementedError: If the pooling type is not implemented.
+    """
+    pooling_layer_dict = {
+        "sum": SumPooling,
+        "mean": AvgPooling,
+        "max": MaxPooling,
+    }
+    try:
+        return pooling_layer_dict[pooling_type]()
+    except KeyError as err:
+        raise NotImplementedError(
+            f"Graph pooling type '{pooling_type}' is not implemented."
+        ) from err
+
+
+def aggregate_tensors(
+    tensor_data: Tensor,
+    aggregation_method: str = "cat",
+    dim: int = -1,
+    weights: Tensor | None = None,
+) -> Tensor:
+    """Aggregates tensor data according to the specified aggregation method.
+
+    Args:
+        tensor_data (Tensor): The tensor data to be aggregated.
+        aggregation_method (str, optional): The method used to aggregate tensor data.
+            Defaults to "cat".
+        dim (int, optional): The dimension along which the aggregation is performed.
+            Defaults to -1.
+        weights (Optional[Tensor], optional): The weights for the weighted sum
+            aggregation method. Required if the aggregation method is 'wsum'. Defaults to None.
+
+    Returns:
+        Tensor: The aggregated tensor data.
+
+    Raises:
+        NotImplementedError: If the aggregation method is not implemented.
+    """
+
+    def wsum(tensor_data: Tensor, weights: Tensor, dim: int = -1) -> Tensor:
+        if weights is None:
+            raise ValueError("weights cannot be None for weighted sum aggregation.")
+
+        weighted_tensor = torch.mul(tensor_data, weights)
+        return torch.sum(weighted_tensor, dim=dim)
+
+    aggregation_method_dict = {
+        "mean": partial(torch.mean, dim=dim),
+        "max": partial(torch.max, dim=dim),
+        "sum": partial(torch.sum, dim=dim),
+        "wsum": partial(wsum, weights=weights, dim=dim),
+    }
+
+    if aggregation_method not in aggregation_method_dict and aggregation_method != "cat":
+        raise NotImplementedError(f"Aggregation method '{aggregation_method}' is not implemented.")
+
+    if aggregation_method == "cat":
+        return torch.cat(tensor_data.unbind(dim=-1), dim=dim)
+    else:
+        aggregation_func = aggregation_method_dict.get(aggregation_method, lambda x: x)
+        return aggregation_func(tensor_data)
--- deep_neuronmorpho/models/modules.py
+++ deep_neuronmorpho/models/modules.py
@@ -1,10 +1,9 @@
 """Modules used to create various models."""
 
-from typing import Any
 
-import torch
-from dgl.nn.pytorch.conv import GINConv
-from torch import nn
+import dgl
+from dgl.nn.pytorch import GINConv
+from torch import Tensor, nn
 
 
 def linear_block(input_dim: int, output_dim: int) -> nn.Sequential:
@@ -19,8 +18,14 @@ def linear_block(input_dim: int, output_dim: int) -> nn.Sequential:
 class MLP(nn.Module):
     """Simple MLP implementation.
 
+    Args:
+        input_dim (int): Input dimension.
+        hidden_dim (int): Hidden layer dimension.
+        output_dim (int): Output dimension.
+        num_layers (int): Number of layers. There are num_layers - 2 hidden layers.
+
     Attributes:
-        layers (nn.ModuleList): List of layers in the MLP.
+        mlp (nn.Sequential): The sequential container of the MLP layers.
 
     Methods:
         forward: Pass inputs through MLP layers.
@@ -33,65 +38,137 @@ class MLP(nn.Module):
         hidden_dim: int,
         num_layers: int,
     ) -> None:
-        """Initialize MLP.
-
-        Args:
-            input_dim (int): Input dimension.
-            hidden_dim (int): Hidden layer dimension.
-            output_dim (int): Output dimension.
-            num_layers (int): Number of layers. There are num_layers - 2 hidden layers.
-
-
-        Raises:
-            ValueError: If num_layers is less than 1.
-        """
         super().__init__()
-
-        self.layers = nn.ModuleList()
 
         if num_layers < 1:
             raise ValueError("Number of layers must be at least 1.")
 
+        layers = []
         if num_layers == 1:  # no hidden layers
-            self.layers.append(linear_block(input_dim, output_dim))
+            layers.append(linear_block(input_dim, output_dim))
         else:  # at least one hidden layer
-            self.layers.append(linear_block(input_dim, hidden_dim))
+            layers.append(linear_block(input_dim, hidden_dim))
             for _layer in range(num_layers - 2):
-                self.layers.append(linear_block(hidden_dim, hidden_dim))
+                layers.append(linear_block(hidden_dim, hidden_dim))
 
-            self.layers.append(linear_block(hidden_dim, output_dim))
+            layers.append(linear_block(hidden_dim, output_dim))
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self.mlp = nn.Sequential(*layers)
+
+    def forward(self, x: Tensor) -> Tensor:
         """Pass inputs through MLP layers.
 
         Args:
-            x (torch.Tensor): Input tensor of shape (batch_size, input_size).
+            x (Tensor): Input tensor of shape (batch_size, input_size).
 
         Returns:
-            torch.Tensor: Output tensor of shape (batch_size, output_size),
+            Tensor: Output tensor of shape (batch_size, output_size),
             where output_size is the size of the last layer in the MLP.
         """
-        for layer in self.layers:
-            x = layer(x)
+        return self.mlp(x)
+
+
+class GINBlock(nn.Module):
+    """GINBlock represents a single GIN layer in the GNN.
+
+    It applies the GINConv operation, followed by batch normalization, activation, and dropout.
+
+    Args:
+        input_dim (int): The input feature dimension.
+        hidden_dim (int): The hidden feature dimension.
+        num_mlp_layers (int): The number of layers in the MLP used in GINConv.
+        aggregator_type (str): The aggregator type used in GINConv (e.g., 'sum', 'mean', or 'max').
+        dropout_prob (float): The probability of dropout.
+        learn_eps (bool, optional): Whether to learn the epsilon parameter in GINConv.
+        Defaults to False.
+
+    Attributes:
+        node_mlp (MLP): The MLP used in GINConv.
+        gin_conv (GINConv): The GINConv layer.
+        bn (nn.BatchNorm1d): The batch normalization layer.
+        activation (nn.ReLU): The activation function (ReLU).
+        dropout (nn.Dropout): The dropout layer.
+    """
+
+    def __init__(
+        self,
+        input_dim: int,
+        hidden_dim: int,
+        num_mlp_layers: int,
+        aggregator_type: str,
+        dropout_prob: float,
+        learn_eps: bool = False,
+    ):
+        super().__init__()
+
+        self.node_mlp = MLP(
+            input_dim=input_dim,
+            output_dim=hidden_dim,
+            hidden_dim=hidden_dim,
+            num_layers=num_mlp_layers,
+        )
+        self.gin_conv = GINConv(self.node_mlp, aggregator_type, init_eps=0, learn_eps=learn_eps)
+        self.bn = nn.BatchNorm1d(hidden_dim)
+        self.activation = nn.ReLU()
+        self.dropout = nn.Dropout(p=dropout_prob)
+
+    def forward(
+        self,
+        x: Tensor,
+        graph: dgl.DGLGraph,
+        edge_weight: Tensor | None = None,
+    ) -> Tensor:
+        """Forward pass of the GINBlock.
+
+        Args:
+            x (Tensor): The input node features.
+            graph (dgl.DGLGraph): The input graph.
+            edge_weight (Tensor, optional): The edge weights. Defaults to None.
+
+        Returns:
+            Tensor: The output node features after applying the GINBlock.
+        """
+        x = self.gin_conv(graph, x, edge_weight)
+        x = self.bn(x)
+        x = self.activation(x)
+        x = self.dropout(x)
         return x
 
 
-def gin_block(
+def create_gnn_layers(
+    num_gnn_layers: int,
     input_dim: int,
-    output_dim: int,
     hidden_dim: int,
     num_mlp_layers: int,
-    aggregator_type: str,
-    **kwargs: Any,
-) -> nn.Sequential:
-    """GNN block."""
-    node_mlp = MLP(
-        input_dim=input_dim,
-        output_dim=output_dim,
-        hidden_dim=hidden_dim,
-        num_layers=num_mlp_layers,
-    )
-    return nn.Sequential(
-        GINConv(node_mlp, aggregator_type, **kwargs),
-        nn.BatchNorm1d(output_dim),
-    )
+    aggregation_type: str,
+    dropout_prob: float,
+    learn_eps: bool,
+) -> nn.ModuleList:
+    """Create a list of GNN layers (GINBlocks) for a given configuration.
+
+    Args:
+        num_gnn_layers (int): The number of GNN layers to create.
+        input_dim (int): The input dimension of the first GINBlock layer.
+        hidden_dim (int): The hidden dimension for all GINBlock layers.
+        num_mlp_layers (int): The number of MLP layers within each GINBlock.
+        aggregation_type (str): The type of aggregation for each GINBlock.
+        dropout_prob (float): The dropout probability for each GINBlock.
+        learn_eps (bool): Whether to learn the epsilon parameter in each GINBlock.
+
+    Returns:
+        nn.ModuleList: A list of GNN layers (GINBlocks) with the given configuration.
+    """
+    gnn_layers = nn.ModuleList()
+    for layer in range(num_gnn_layers):
+        input_dim = input_dim if layer == 0 else hidden_dim
+        gnn_layers.append(
+            GINBlock(
+                input_dim=input_dim,
+                hidden_dim=hidden_dim,
+                num_mlp_layers=num_mlp_layers,
+                aggregator_type=aggregation_type,
+                dropout_prob=dropout_prob,
+                learn_eps=learn_eps,
+            )
+        )
+    return gnn_layers
