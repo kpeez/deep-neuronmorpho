098f438c05bd7791b86c738661b5a6b1ad945a8c
Kyle Puhger <krpuhger@gmail.com>
2023-04-27 17:07:52 -0700

    Implement MACGNN model (#6)
    * feat: Add `compute_embedding_dim` function
    
    - Added compute_embedding_dim function to compute the
    embedding dimension based on the given model parameters.
    
    * set stream aggregation to wsum
    
    * rename `create_gnn_layers` to `create_gin_layers`
    
    - renamed function to be more specific
    
    * feat: Add MACGNN model implementation
    
    Implement the MACGNN deep learning model as described
    in [Zhao et al. 2022](https://ieeexplore.ieee.org/document/9895206),
    following the provided architecture and configuration options.
    The implementation includes the necessary utility functions and support
    for multiple attribute streams, aggregation methods, and GNN layers.

---
+++ deep_neuronmorpho/models/macgnn.py
@@ -0,0 +1,131 @@
+"""Implementation of Morphology-aware contrastive GNN model (MACGNN) model from [Zhao et al. 2022](https://ieeexplore.ieee.org/document/9895206).
+
+## Model architecture:
+- GIN layers for proecssing graph features
+- Graph pooling (can specify either "mean", "max", or "sum" pooling)
+- Two or more attribute streams (e.g. geometric attributes, topological attributes, etc.)
+- Stream aggregation (can specify either "mean", "max", "sum", "wsum", or "cat" aggregation)
+    - "wsum" aggregation uses a learnable weight for each stream
+- MLP layer processes the aggregated stream features to produce final graph-level embedding
+"""
+
+import torch
+from dgl import DGLGraph
+from torch import Tensor, nn
+
+from deep_neuronmorpho.models.model_utils import (
+    aggregate_tensor,
+    compute_embedding_dim,
+    create_pooling_layer,
+    load_attrs_streams,
+)
+from deep_neuronmorpho.models.modules import create_gin_layers, linear_block
+from deep_neuronmorpho.utils.parse_config import ModelConfig
+
+
+class MACGNN(nn.Module):
+    """MACGNN model from [Zhao et al. 2022](https://ieeexplore.ieee.org/document/9895206)."""
+
+    def __init__(self, args: ModelConfig) -> None:
+        super().__init__()
+
+        self.args = args
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.use_edge_weight = args.model.use_edge_weight
+        self.learn_eps = args.model.learn_eps
+        self.hidden_dim = args.model.hidden_dim
+        self.output_dim = args.model.output_dim
+        self.num_mlp_layers = args.model.num_mlp_layers
+        self.num_gnn_layers = args.model.num_gnn_layers
+        self.graph_pooling_type = args.model.graph_pooling_type
+        self.neighbor_aggregation = args.model.neighbor_aggregation
+        self.gnn_layers_aggregation = args.model.gnn_layers_aggregation
+        self.stream_aggregation = args.model.stream_aggregation
+        self.dropout_prob = args.training.dropout_prob
+        self.attrs_streams = load_attrs_streams(args.model.attrs_streams.to_dict())
+        self.num_streams = len(self.attrs_streams)
+        self.streams_weights = None
+        self.gnn_streams = nn.ModuleDict()
+
+        # Initialize the GNN layers
+        for stream_name, stream_dims in self.attrs_streams.items():
+            input_dim = len(stream_dims)
+            self.gnn_streams[stream_name] = create_gin_layers(
+                num_gnn_layers=self.num_gnn_layers,
+                input_dim=input_dim,
+                hidden_dim=self.hidden_dim,
+                num_mlp_layers=self.num_mlp_layers,
+                aggregation_type=self.neighbor_aggregation,
+                dropout_prob=self.dropout_prob,
+                learn_eps=self.learn_eps,
+            )
+        self.gpool = create_pooling_layer(self.graph_pooling_type)
+
+        # Initialize stream weights if using weighted sum for streams aggregation
+        if self.stream_aggregation == "wsum" and self.num_streams > 1:
+            self.streams_weight = nn.Parameter(
+                torch.ones(1, 1, self.num_streams), requires_grad=True
+            )
+            nn.init.xavier_uniform_(self.streams_weight)
+
+    def process_stream(
+        self, stream_name: str, h_full: Tensor, graphs: DGLGraph, edge_weight: Tensor | None
+    ) -> Tensor:
+        """Process an attribute stream.
+
+        Processes an attribute stream with its corresponding GNN layers,
+        and aggregate features from all layers in th stream to get graph-level representation.
+
+        Args:
+            stream_name (str): Name of the attribute stream in self.attrs_streams dict.
+            h_full (Tensor): Node features of the graph.
+            graphs (DGLGraph): Batch of graph objects from dgl.GraphDataLoader.
+            edge_weight (Tensor | None): Edge weights of the graph. Defaults to None.
+
+
+        Returns:
+            Tensor: Graph-level representation of the attribute stream.
+        """
+        gnn_layers = self.gnn_streams[stream_name]
+        stream_indices = self.attrs_streams[stream_name]
+        h = h_full[:, stream_indices]
+
+        h_layers_list = []
+        for i in range(self.num_gnn_layers):
+            h = gnn_layers[i](h, graphs, edge_weight)
+            h_layers_list.append(h)
+
+        h_graph_rep_stream = torch.stack([self.gpool(graphs, h) for h in h_layers_list], dim=-1)
+        return aggregate_tensor(h_graph_rep_stream, self.gnn_layers_aggregation)
+
+    def forward(self, graphs: DGLGraph) -> Tensor:
+        """Forward pass of the model."""
+        h_full = graphs.ndata["nattrs"]
+        edge_weight = graphs.edata["edge_weight"] if self.use_edge_weight else None
+
+        h_streams_list = []
+        for stream_name in self.gnn_streams:
+            h_graph_rep_stream = self.process_stream(stream_name, h_full, graphs, edge_weight)
+            h_streams_list.append(h_graph_rep_stream)
+
+        # Aggregate across streams
+        h_concat_streams = torch.stack(h_streams_list, dim=-1)
+        stream_aggregate_graph_rep = aggregate_tensor(
+            h_concat_streams,
+            self.stream_aggregation,
+            weights=self.streams_weight,  # only used if stream_aggregation == "wsum"
+        )
+        embedding_dim = compute_embedding_dim(
+            hidden_dim=self.hidden_dim,
+            num_gnn_layers=self.num_gnn_layers,
+            num_streams=self.num_streams,
+            gnn_layers_aggregation=self.gnn_layers_aggregation,
+            stream_aggregation=self.stream_aggregation,
+        )
+        final_graph_rep = stream_aggregate_graph_rep
+        final_graph_rep = linear_block(
+            input_dim=embedding_dim,
+            output_dim=self.output_dim,
+        )(stream_aggregate_graph_rep)
+
+        return final_graph_rep
--- deep_neuronmorpho/models/model_utils.py
+++ deep_neuronmorpho/models/model_utils.py
@@ -7,51 +7,7 @@ from dgl.nn.pytorch import AvgPooling, MaxPooling, SumPooling
 from torch import Tensor, nn
 
 
-def load_attrs_streams(attrs_streams: dict[str, str]) -> dict[str, list[int]]:
-    """Load attribute streams from a dictionary containing the range of indices.
-
-    Args:
-        attrs_streams (dict[str, str]): A dictionary where the keys are stream names,
-        and the values are a tuple the index range (inclusive) in string format.
-
-    Returns:
-        dict[str, list[int]]: A dictionary where the keys are stream names,
-        and the values are lists of indices corresponding to the given index range.
-    """
-    attrs_idxs = {}
-    for name, stream in attrs_streams.items():
-        start, end = eval(stream)
-        attrs_idxs[name] = list(range(start, end + 1))
-
-    return attrs_idxs
-
-
-def create_pooling_layer(pooling_type: str) -> nn.Module:
-    """Create a pooling layer for the given pooling type.
-
-    Args:
-        pooling_type (str): The pooling type ('sum', 'mean', or 'max').
-
-    Returns:
-        nn.Module: The created pooling layer.
-
-    Raises:
-        NotImplementedError: If the pooling type is not implemented.
-    """
-    pooling_layer_dict = {
-        "sum": SumPooling,
-        "mean": AvgPooling,
-        "max": MaxPooling,
-    }
-    try:
-        return pooling_layer_dict[pooling_type]()
-    except KeyError as err:
-        raise NotImplementedError(
-            f"Graph pooling type '{pooling_type}' is not implemented."
-        ) from err
-
-
-def aggregate_tensors(
+def aggregate_tensor(
     tensor_data: Tensor,
     aggregation_method: str = "cat",
     dim: int = -1,
@@ -97,3 +53,79 @@ def aggregate_tensors(
     else:
         aggregation_func = aggregation_method_dict.get(aggregation_method, lambda x: x)
         return aggregation_func(tensor_data)
+
+
+def create_pooling_layer(pooling_type: str) -> nn.Module:
+    """Create a pooling layer for the given pooling type.
+
+    Args:
+        pooling_type (str): The pooling type ('sum', 'mean', or 'max').
+
+    Returns:
+        nn.Module: The created pooling layer.
+
+    Raises:
+        NotImplementedError: If the pooling type is not implemented.
+    """
+    pooling_layer_dict = {
+        "sum": SumPooling,
+        "mean": AvgPooling,
+        "max": MaxPooling,
+    }
+    try:
+        return pooling_layer_dict[pooling_type]()
+    except KeyError as err:
+        raise NotImplementedError(
+            f"Graph pooling type '{pooling_type}' is not implemented."
+        ) from err
+
+
+def compute_embedding_dim(
+    hidden_dim: int,
+    num_gnn_layers: int,
+    num_streams: int,
+    gnn_layers_aggregation: str,
+    stream_aggregation: str,
+) -> int:
+    """Computes the embedding dimension based on the given parameters.
+
+    Args:
+        hidden_dim (int): The dimensionality of the hidden layers in the GNN.
+        num_gnn_layers (int): The number of GNN layers in the model.
+        num_streams (int): The number of input streams.
+        gnn_layers_aggregation (str): The aggregation method used for the GNN layers.
+        Possible values are ["sum", "mean", "max", "wsum", "cat"].
+        stream_aggregation (str): The aggregation method used for the input streams.
+        Possible values are ["sum", "mean", "max", "wsum", "cat"].
+
+    Returns:
+        int: The embedding dimension computed based on the given parameters.
+    """
+    if gnn_layers_aggregation == "cat" and stream_aggregation == "cat":
+        embedding_dim = hidden_dim * num_gnn_layers * num_streams
+    elif gnn_layers_aggregation == "cat":
+        embedding_dim = hidden_dim * num_gnn_layers
+    elif stream_aggregation == "cat":
+        embedding_dim = hidden_dim * num_streams
+    else:
+        embedding_dim = hidden_dim
+    return embedding_dim
+
+
+def load_attrs_streams(attrs_streams: dict[str, str]) -> dict[str, list[int]]:
+    """Load attribute streams from a dictionary containing the range of indices.
+
+    Args:
+        attrs_streams (dict[str, str]): A dictionary where the keys are stream names,
+        and the values are a tuple the index range (inclusive) in string format.
+
+    Returns:
+        dict[str, list[int]]: A dictionary where the keys are stream names,
+        and the values are lists of indices corresponding to the given index range.
+    """
+    attrs_idxs = {}
+    for name, stream in attrs_streams.items():
+        start, end = eval(stream)
+        attrs_idxs[name] = list(range(start, end + 1))
+
+    return attrs_idxs
