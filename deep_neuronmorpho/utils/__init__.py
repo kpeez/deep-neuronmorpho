22edf8442658342b183762ac6ff1a02371d78040
kpuhger <krpuhger@gmail.com>
2023-05-14 09:57:42 -0700

    add: logging setup helper function.
    - adds function to configure logger

--- deep_neuronmorpho/utils/__init__.py
+++ deep_neuronmorpho/utils/__init__.py
@@ -1,8 +1,4 @@
+from .monitoring import ProgressBar, setup_logger
 from .parse_config import ModelConfig, validate_model_config
-from .progress import ProgressBar
 
-__all__ = [
-    "ModelConfig",
-    "validate_model_config",
-    "ProgressBar",
-]
+__all__ = ["ModelConfig", "validate_model_config", "ProgressBar", "setup_logger"]
