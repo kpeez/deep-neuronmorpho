c6967654281191219546f4fb84e24bc8077530d3
kpuhger <krpuhger@gmail.com>
2023-05-25 17:04:29 -0700

    import evaluation-related functions

--- deep_neuronmorpho/engine/__init__.py
+++ deep_neuronmorpho/engine/__init__.py
@@ -1,7 +1,10 @@
+from .evaluation import evaluate_embeddings, get_evaluation_targets
 from .ntxent_loss import NTXEntLoss
 from .trainer import ContrastiveTrainer
 
 __all__ = [
     "ContrastiveTrainer",
     "NTXEntLoss",
+    "evaluate_embeddings",
+    "get_evaluation_targets",
 ]
