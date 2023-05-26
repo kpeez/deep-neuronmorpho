3de20445aa94603c204dc8f0849e07b717696a93
kpuhger <krpuhger@gmail.com>
2023-05-25 21:13:54 -0700

    import `TrainLogger`

--- deep_neuronmorpho/utils/__init__.py
+++ deep_neuronmorpho/utils/__init__.py
@@ -1,4 +1,9 @@
-from .monitoring import ProgressBar, setup_logger
+from .monitoring import ProgressBar, TrainLogger
 from .parse_config import ModelConfig, validate_model_config
 
-__all__ = ["ModelConfig", "validate_model_config", "ProgressBar", "setup_logger"]
+__all__ = [
+    "ModelConfig",
+    "validate_model_config",
+    "ProgressBar",
+    "TrainLogger",
+]
