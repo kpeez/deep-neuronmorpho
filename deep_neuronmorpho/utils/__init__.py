11986f2fb001193b21f779fb9fd60e897728a8b4
kpuhger <krpuhger@gmail.com>
2023-05-28 17:38:46 -0700

    remove validate_model_config from __all__

--- deep_neuronmorpho/utils/__init__.py
+++ deep_neuronmorpho/utils/__init__.py
@@ -1,9 +1,8 @@
 from .monitoring import ProgressBar, TrainLogger
-from .parse_config import ModelConfig, validate_model_config
+from .parse_config import ModelConfig
 
 __all__ = [
     "ModelConfig",
-    "validate_model_config",
     "ProgressBar",
     "TrainLogger",
 ]
