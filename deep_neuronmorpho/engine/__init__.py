2ed2a6380caeb6b87997fbe7031171a514632391
kpuhger <krpuhger@gmail.com>
2023-09-30 17:46:36 -0700

    add Checkpoint

--- deep_neuronmorpho/engine/__init__.py
+++ deep_neuronmorpho/engine/__init__.py
@@ -1,4 +1,4 @@
 from .trainer import ContrastiveTrainer
-from .trainer_utils import setup_dataloaders
+from .trainer_utils import Checkpoint, setup_dataloaders
 
-__all__ = ["ContrastiveTrainer", "setup_dataloaders"]
+__all__ = ["ContrastiveTrainer", "setup_dataloaders", "Checkpoint"]
