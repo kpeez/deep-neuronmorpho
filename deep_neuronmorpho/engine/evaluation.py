6b61a3168213b31265b879f40489b0d20ebe7190
kpuhger <krpuhger@gmail.com>
2023-10-31 14:07:48 -0700

    replace XGBoost with SVM, add grid search for margin parameter by default

--- deep_neuronmorpho/engine/evaluation.py
+++ deep_neuronmorpho/engine/evaluation.py
@@ -5,9 +5,10 @@ import re
 import dgl
 import pandas as pd
 from numpy.typing import NDArray
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
 from torch import nn
-from xgboost import XGBClassifier
 
 from deep_neuronmorpho.data import NeuronGraphDataset
 
@@ -15,20 +16,24 @@ from deep_neuronmorpho.data import NeuronGraphDataset
 def evaluate_embeddings(
     embeddings: dict[str, NDArray],
     targets: dict[str, NDArray],
+    search: bool = True,
     random_state: int | None = None,
 ) -> float:
-    """Evaluate the quality of the contrastive learning embeddings using XGBoost classifier.
+    """Evaluate the quality of the contrastive learning embeddings on benchmark classification task.
 
     Args:
         embeddings (dict[str, NDArray]): Dictionary of embeddings with keys "train" and "test".
             Each value should be a 2D array-like object (samples, embeddings).
         targets (dict[str, str]): Dictionary of targets with keys "train" and "test".
             Each value should be a 1D array-like object of categorical string labels.
-        random_state (int, optional): Determines random number generation for XGBoost.
-            Use an int for reproducible results. If None, the randomness will depend on np.random.
+        search (bool, optional): Whether to perform a grid search for hyperparameter tuning.
+        Defaults to True.
+        random_state (int, optional): Controls the pseudo random number generation for shuffling the
+        data for probability estimates.
+        Pass an int for reproducible output across multiple function calls. Defaults to None.
 
     Returns:
-        float: Mean accuracy of the XGBoost classifier on the test set.
+        float: Mean accuracy of the classifier on the test set.
     """
     train_embeddings, test_embeddings = embeddings["train"], embeddings["test"]
     train_targets, test_targets = targets["train"], targets["test"]
@@ -37,10 +42,16 @@ def evaluate_embeddings(
     train_labels = label_encoder.transform(train_targets)
     test_labels = label_encoder.transform(test_targets)
 
-    clf_xgb = XGBClassifier(random_state=random_state)
-    clf_xgb.fit(train_embeddings, train_labels)
+    if search:
+        params = {"C": [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+        clf = GridSearchCV(SVC(gamma="auto"), params, cv=5, scoring="accuracy", verbose=0)
 
-    return clf_xgb.score(test_embeddings, test_labels)
+    else:
+        clf = SVC(gamma="auto")
+
+    clf.fit(train_embeddings, train_labels)
+
+    return clf.score(test_embeddings, test_labels)
 
 
 def create_embedding_df(dataset: NeuronGraphDataset, model: nn.Module) -> pd.DataFrame:
