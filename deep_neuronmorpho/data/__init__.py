e2d9759ee2272306541bed1ae22153410065d7e4
kpuhger <krpuhger@gmail.com>
2024-02-09 12:32:18 -0800

    fix relative imports

--- deep_neuronmorpho/data/__init__.py
+++ deep_neuronmorpho/data/__init__.py
@@ -1,5 +1,5 @@
-from .create_dataset import GraphScaler, NeuronGraphDataset
-from .graph_augmentation import GraphAugmenter
-from .process_swc import SWCData
+from deep_neuronmorpho.data.create_dataset import GraphScaler, NeuronGraphDataset
+from deep_neuronmorpho.data.graph_augmentation import GraphAugmenter
+from deep_neuronmorpho.data.process_swc import SWCData
 
 __all__ = ["GraphAugmenter", "NeuronGraphDataset", "GraphScaler", "SWCData"]
