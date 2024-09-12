9e672e17e628ae14df1572e80d84c581929076d3
kpuhger <krpuhger@gmail.com>
2024-09-12 16:37:33 -0700

    refactor to use lightning-based trainer and modules

--- deep_neuronmorpho/train.py
+++ deep_neuronmorpho/train.py
@@ -1,76 +1,41 @@
-from pathlib import Path
-
 import pytorch_lightning as pl
 import torch
 import typer
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 
 from deep_neuronmorpho.engine import (
     ContrastiveGraphModule,
-    NTXEntLoss,
+    create_loss_fn,
+    create_model,
+    create_trainer,
+    setup_callbacks,
     setup_dataloaders,
-    setup_experiment_results,
+    setup_logging,
 )
-from deep_neuronmorpho.models import MACGNN, MACGNNv2
 from deep_neuronmorpho.utils import Config
 
 app = typer.Typer()
 
 
-def train_model(
-    config_file: str,
-    checkpoint: str | None = None,
-) -> None:
+def train_model(config_file: str, checkpoint: str | None = None) -> None:
     """Train a model using a configuration file."""
     conf = Config.from_yaml(config_file=config_file)
     if conf.training.random_state is not None:
         pl.seed_everything(conf.training.random_state, workers=True)
-
-    # Generate unique experiment name and directory
-    expt_name, expt_dir = setup_experiment_results(conf)
-
+    logger, ckpts_dir = setup_logging(conf)
+    callbacks = setup_callbacks(conf, ckpts_dir, logger.version)
     dataloaders = setup_dataloaders(
         conf,
         datasets=["contra_train", "eval_train"],
         pin_memory=torch.cuda.is_available(),
         num_workers=9,
     )
-
-    model_checkpoint = ModelCheckpoint(
-        dirpath=Path(expt_dir) / "ckpts",
-        filename=f"{expt_name}-{{step:07d}}-{{train_loss:.2f}}",
-        save_top_k=3,
-        save_last=True,
-        every_n_train_steps=10 * conf.training.logging_steps,
-        monitor="train_loss",
-        mode="min",
-    )
-    early_stopping = (
-        EarlyStopping(
-            monitor="train_loss",
-            patience=conf.training.patience,
-            mode="min",
-        )
-        if conf.training.patience
-        else None
-    )
-
-    # load model and loss function
-    model = MACGNNv2(conf.model) if "v2" in conf.model.name.lower() else MACGNN(conf.model)
-    loss_fn = NTXEntLoss(conf.training.contra_loss_temp)
-    # create training module and trainer
+    model = create_model(conf.model.name, conf.model)
+    # TODO: make parsing loss args more flexible
+    loss_fn = create_loss_fn(conf.training.loss_fn, temp=conf.training.loss_temp)
     contrastive_model = ContrastiveGraphModule(model, conf, loss_fn=loss_fn)
-    trainer = pl.Trainer(
-        # max_epochs=conf.training.epochs,
-        max_steps=conf.training.max_steps,
-        accelerator="gpu" if torch.cuda.is_available() else "cpu",
-        devices="auto",
-        deterministic=True,
-        logger=pl.loggers.TensorBoardLogger(Path(expt_dir) / "logs"),
-        log_every_n_steps=conf.training.logging_steps,
-        callbacks=[model_checkpoint, early_stopping],
-    )
-    # Train the model
+    callbacks = setup_callbacks(conf, ckpts_dir, logger.version)
+    trainer = create_trainer(conf, logger, callbacks)
+
     trainer.fit(
         contrastive_model,
         train_dataloaders=dataloaders["contra_train"],
