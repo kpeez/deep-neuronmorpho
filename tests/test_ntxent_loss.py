5c5b61b9224ba0239a4a58854eef39b3725f6f8a
Kyle Puhger <krpuhger@gmail.com>
2025-08-01 14:21:42 -0700

    clean up dependencies, update morphopy source (#68)
    * ignore hidden directories
    
    * refactor SWC data loading to enforce root node validation
    
    - Updated the logic to ensure any nodes with parent == -1 are set as type 1 (soma).
    - Added an assertion to confirm that the SWC file contains at least one root node with parent == -1
    
    * update gitignore
    
    * remove redundant comments
    
    * update ruff
    
    * remove requirements.txt update lockfile
    
    * remove pip installation instructions
    
    * remove pinned scipy/numpy deps
    
    - morphopy fixed these I think
    
    * update var name
    
    * use git for morphopy source

--- tests/test_ntxent_loss.py
+++ tests/test_ntxent_loss.py
@@ -13,7 +13,7 @@ class TestNTXEntLoss:
     def setup(self) -> None:
         """Set up test fixtures."""
         torch.manual_seed(0)
-        self.loss = NTXEntLoss(temperature=1)
+        self.loss = NTXEntLoss(temp=1)
         self.a = torch.randn(32, 128)
         self.self_loss = self.loss(self.a, self.a)
 
@@ -28,14 +28,14 @@ class TestNTXEntLoss:
 
     def test_ntxent_loss_high_temp(self) -> None:
         """Test NTXEntLoss with high temperature should be larger."""
-        loss_high_temp = NTXEntLoss(temperature=1000.0)
+        loss_high_temp = NTXEntLoss(temp=1000.0)
         self_loss_high = loss_high_temp(self.a, self.a)
         assert self_loss_high.item() > 0
         assert self_loss_high.item() > self.self_loss.item()
 
     def test_ntxent_loss_low_temp(self) -> None:
         """Test NTXEntLoss with low temperature should be less."""
-        loss_low_temp = NTXEntLoss(temperature=0.1)
+        loss_low_temp = NTXEntLoss(temp=0.1)
         self_loss_low = loss_low_temp(self.a, self.a)
         assert self_loss_low.item() > 0
         assert self_loss_low.item() < self.self_loss.item()
