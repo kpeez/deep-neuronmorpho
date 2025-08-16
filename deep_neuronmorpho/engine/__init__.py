c08439450368631702853b7bb82f67d150e10a74
kpuhger <krpuhger@gmail.com>
2025-08-15 23:46:55 -0700

    deprecate trainer_utils
    - no longer needed since hydra manages factory functions

--- deep_neuronmorpho/engine/__init__.py
+++ deep_neuronmorpho/engine/__init__.py
@@ -1,13 +1,3 @@
-from .lightning_modules import ContrastiveGraphModule
+from .data_module import NeuronDataModule
+from .lightning_module import ContrastiveGraphModule
 from .ntxent_loss import NTXEntLoss
-from .trainer_utils import (
-    create_loss_fn,
-    create_model,
-    create_optimizer,
-    create_scheduler,
-    create_trainer,
-    log_hyperparameters,
-    setup_callbacks,
-    setup_dataloaders,
-    setup_logging,
-)
