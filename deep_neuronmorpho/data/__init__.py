32717dfc64538b1d2cf15dc4bbc3a2ac4101cd6a
kpuhger <krpuhger@gmail.com>
2025-08-16 10:42:15 -0700

    revert name to datasets

--- deep_neuronmorpho/data/__init__.py
+++ deep_neuronmorpho/data/__init__.py
@@ -1,5 +1,5 @@
 from .augmentations import augment_graph
-from .dataset import ContrastiveNeuronDataset, NeuronDataset
+from .datasets import ContrastiveNeuronDataset, NeuronDataset
 from .graph_construction import swc_df_to_pyg_data
 from .graph_features import compute_neuron_node_feats
 from .process_swc import SWCData
