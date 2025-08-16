9096356ace2b3bd03855d947553575409adb2945
kpuhger <krpuhger@gmail.com>
2025-08-15 23:56:53 -0700

    simplify training script by using hydra config

--- deep_neuronmorpho/train.py
+++ deep_neuronmorpho/train.py
@@ -1,58 +1,31 @@
+import hydra
 import pytorch_lightning as pl
-import torch
-import typer
+from hydra.utils import instantiate
+from omegaconf import DictConfig
 
 from deep_neuronmorpho.engine import (
     ContrastiveGraphModule,
-    create_model,
-    create_trainer,
-    log_hyperparameters,
-    setup_callbacks,
-    setup_dataloaders,
-    setup_logging,
+    NeuronDataModule,
 )
-from deep_neuronmorpho.utils import Config
 
 
-def train_model(config_file: str, checkpoint: str | None = None) -> None:
+@hydra.main(config_path="../conf", config_name="config", version_base=None)
+def train_model(cfg: DictConfig) -> None:
     """Train a model using a configuration file."""
-    cfg = Config.load(config_file=config_file)
     if cfg.training.random_state is not None:
         pl.seed_everything(cfg.training.random_state, workers=True)
-    logger, ckpts_dir = setup_logging(cfg)
-
-    log_hyperparameters(logger, cfg)
-    callbacks = setup_callbacks(cfg, ckpts_dir)
-    dataloaders = setup_dataloaders(
-        cfg,
-        datasets=["contra_train", "eval_train"],
-        pin_memory=torch.cuda.is_available(),
-        num_workers=9,
-    )
-    model = create_model(cfg.model.name, cfg.model)
+    model = instantiate(cfg.model)
+    # TODO: add loss function to lightning module
     # loss_fn = create_loss_fn(cfg.training.loss_fn, temp=cfg.training.loss_temp)
-    contrastive_model = ContrastiveGraphModule(model, cfg)
-    trainer = create_trainer(cfg, logger, callbacks)
-
+    contrastive_module: pl.LightningModule = ContrastiveGraphModule(model, cfg)
+    datamodule: pl.LightningDataModule = NeuronDataModule(cfg)
+    trainer: pl.Trainer = instantiate(cfg.training.trainer)
     trainer.fit(
-        contrastive_model,
-        train_dataloaders=dataloaders["contra_train"],
-        val_dataloaders=dataloaders["eval_train"],
-        ckpt_path=checkpoint,
+        contrastive_module,
+        datamodule=datamodule,
+        ckpt_path=None,
     )
 
 
 if __name__ == "__main__":
-    app = typer.Typer()
-
-    @app.command()
-    def cli_train_model(
-        config_file: str = typer.Argument(..., help="The configuration file."),
-        checkpoint: str = typer.Option(
-            None, "--checkpoint", "-c", help="The checkpoint file to load from."
-        ),
-    ) -> None:
-        """CLI for training a model using a configuration file."""
-        train_model(config_file, checkpoint)
-
-    app()
+    train_model()
