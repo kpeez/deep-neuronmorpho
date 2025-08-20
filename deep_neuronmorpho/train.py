a06ca99ac05758eb1e34e315c200ec8b34f6c37d
kpuhger <krpuhger@gmail.com>
2025-08-20 14:23:59 -0700

    update training script

--- deep_neuronmorpho/train.py
+++ deep_neuronmorpho/train.py
@@ -3,25 +3,18 @@ import pytorch_lightning as pl
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 
-from deep_neuronmorpho.engine import (
-    ContrastiveGraphModule,
-    NeuronDataModule,
-)
-
 
 @hydra.main(config_path="../conf", config_name="config", version_base=None)
 def train_model(cfg: DictConfig) -> None:
     """Train a model using a configuration file."""
     if cfg.training.random_state is not None:
         pl.seed_everything(cfg.training.random_state, workers=True)
-    model = instantiate(cfg.model)
-    # TODO: add loss function to lightning module
-    # loss_fn = create_loss_fn(cfg.training.loss_fn, temp=cfg.training.loss_temp)
-    contrastive_module: pl.LightningModule = ContrastiveGraphModule(model, cfg)
-    datamodule: pl.LightningDataModule = NeuronDataModule(cfg)
-    trainer: pl.Trainer = instantiate(cfg.training.trainer)
+
+    lightning_module = instantiate(cfg.training.lightning_module)
+    datamodule = instantiate(cfg.datamodule)
+    trainer = instantiate(cfg.trainer)
     trainer.fit(
-        contrastive_module,
+        lightning_module,
         datamodule=datamodule,
         ckpt_path=None,
     )
