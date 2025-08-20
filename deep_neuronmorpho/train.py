32a1858fd419fbcee8d2175a655e26ce7d2ba09d
kpuhger <krpuhger@gmail.com>
2025-08-20 16:01:33 -0700

    use cfg.seed for setting, add cfg.ckpt_path

--- deep_neuronmorpho/train.py
+++ deep_neuronmorpho/train.py
@@ -7,16 +7,16 @@ from omegaconf import DictConfig
 @hydra.main(config_path="../conf", config_name="config", version_base=None)
 def train_model(cfg: DictConfig) -> None:
     """Train a model using a configuration file."""
-    if cfg.training.random_state is not None:
-        pl.seed_everything(cfg.training.random_state, workers=True)
+    if cfg.seed is not None:
+        pl.seed_everything(cfg.seed, workers=True)
 
     lightning_module = instantiate(cfg.training.lightning_module)
-    datamodule = instantiate(cfg.datamodule)
-    trainer = instantiate(cfg.trainer)
+    datamodule = instantiate(cfg.data)
+    trainer = instantiate(cfg.training.trainer)
     trainer.fit(
         lightning_module,
         datamodule=datamodule,
-        ckpt_path=None,
+        ckpt_path=cfg.ckpt_path,
     )
 
 
